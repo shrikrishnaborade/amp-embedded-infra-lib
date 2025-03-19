a6007e09493d4f4ffb85b93a368ae27838aef611
shrikrishnaborade <shrikrishna.borade@philips.com>
2025-03-19 15:01:30 +0100

    Task: LWIP: support for PPPoS

--- lwip/lwip_config/lwipopts.h
+++ lwip/lwip_config/lwipopts.h
@@ -18,9 +18,9 @@ extern "C"
 #define LWIP_AUTOIP 1
 #define LWIP_DHCP_AUTOIP_COOP 1
 #define LWIP_DNS 1
-#define LWIP_RAW 0
+#define LWIP_RAW 1
 #define LWIP_IGMP 1
-uint32_t StaticLwIpRand();
+    uint32_t StaticLwIpRand();
 #define LWIP_RAND() StaticLwIpRand()
 #define MEM_ALIGNMENT 4
 #define LWIP_STATS 0
@@ -29,13 +29,13 @@ uint32_t StaticLwIpRand();
 
 #define MEM_SIZE 3200
 
-#define CHECKSUM_GEN_IP 0
-#define CHECKSUM_GEN_UDP 0
-#define CHECKSUM_GEN_TCP 0
-#define CHECKSUM_GEN_ICMP 0
-#define CHECKSUM_CHECK_IP 0
-#define CHECKSUM_CHECK_UDP 0
-#define CHECKSUM_CHECK_TCP 0
+#define CHECKSUM_GEN_IP 1
+#define CHECKSUM_GEN_UDP 1
+#define CHECKSUM_GEN_TCP 1
+#define CHECKSUM_GEN_ICMP 1
+#define CHECKSUM_CHECK_IP 1
+#define CHECKSUM_CHECK_UDP 1
+#define CHECKSUM_CHECK_TCP 1
 
 #define LWIP_NETIF_HOSTNAME 1
 #define LWIP_NETIF_EXT_STATUS_CALLBACK 1
@@ -43,7 +43,22 @@ uint32_t StaticLwIpRand();
 
 #define LWIP_TCP_PCB_NUM_EXT_ARGS 1
 
-#define LWIP_DHCP_AUTOIP_COOP_TRIES 5
+// PPP
+#define PPP_SUPPORT 1
+#define PAP_SUPPORT 1  /* Set > 0 for PAP. */
+#define CHAP_SUPPORT 1 /* Set > 0 for CHAP. */
+#define PPP_SERVER 1
+#define PPP_NOTIFY_PHASE 1
+#define LWIP_NETIF_STATUS_CALLBACK 1
+#define LWIP_NETIF_LINK_CALLBACK 1
+// debug traces
+#define LWIP_DEBUG 1
+#define PPP_DEBUG LWIP_DBG_ON
+#define NETIF_DEBUG LWIP_DBG_ON
+#define SLIP_DEBUG LWIP_DBG_ON
+#define IP6_DEBUG LWIP_DBG_ON
+#define DHCP6_DEBUG LWIP_DBG_ON
+#define PRINTPKT_SUPPORT 1
 
 #ifdef __cplusplus
 }
